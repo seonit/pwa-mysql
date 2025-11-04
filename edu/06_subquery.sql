a7dfb0413faf55617a2603f524e74dac458994d7
seonit <seonit08@gmail.com>
2025-11-04 18:11:44 +0900

    251104 subquery~join, tng1

--- edu/06_subquery.sql
+++ edu/06_subquery.sql
@@ -72,3 +72,58 @@ WHERE
 			department_managers.emp_id = employees.emp_id
 	)
 ;
+
+-- -----------------
+-- SELECT절에서 사용
+-- -----------------
+-- 사원별 역대 전체 급여 평균
+SELECT
+	emp.emp_id
+	,(
+		SELECT AVG(sal.salary)
+		FROM salaries sal
+		WHERE emp.emp_id = sal.emp_id
+	) avg_sal
+FROM employees emp
+;
+
+-- -----------------
+-- FROM절에서 사용
+-- -----------------
+SELECT
+	tmp.*
+FROM (
+	SELECT
+		emp.emp_id
+		,emp.`name`
+	FROM employees emp
+) tmp
+;
+-- ------------------
+-- INSERT문에서 사용
+-- ------------------
+INSERT INTO title_emps(
+	emp_id
+	,title_code
+	,start_at
+)
+VALUES(
+	(SELECT MAX(emp_id) FROM employees)
+	,(SELECT title_code FROM titles WHERE title = '사원')
+	,DATE(NOW())	
+);
+
+-- -----------------
+-- UPDATE문에서 사용
+-- -----------------
+UPDATE title_emps tite1
+SET
+	tite1.end_at = (
+		SELECT emp.fire_at
+		FROM employees emp
+		WHERE emp.emp_id = 100000
+	)
+WHERE
+	tite1.emp_id = 100000
+	AND tite1.end_at IS NULL
+;
